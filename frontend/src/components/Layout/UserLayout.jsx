9d134bcb1a711b97319864313612962c07c12ddb
Rahul Verma <verma.rahul1441v@gmail.com>
2025-03-25 20:30:53 +0530

    added footer section

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../Common/Header';
+import Footer from '../Common/Footer';
 const UserLayout = () => {
   return (
     <>
       {/* Header */}
       <Header/> 
       {/* Main content */}
-      {/* Footer */}
       
+      {/* Footer */}
+      <Footer />
     </>
   );
 };
